26a5d14a7659445273a0fcd8ecf3d83f3d743138
m.hevin <matteo.hevin@wiilog.fr>
2021-01-27 12:35:35 +0100

    iot namespace secret

--- setup/iot.sh
+++ setup/iot.sh
@@ -3,6 +3,9 @@ function initialize_iot() {
     echo ""
     echo "Namespace creation...."
     kubectl apply -f ../configs/iot/namespace.yml
+    echo "Secret creation...."
+    kubectl apply -f ../configs/iot/secret.yml
+    echo ""
 }
 
 initialize_iot
