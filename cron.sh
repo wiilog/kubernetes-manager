8180f5da1f4e6c9d4c8097a64d2d22bfca54a760
Marwane <marwane.benoukaiss@wiilog.fr>
2021-05-24 10:57:39 +0200

    Collins every minute dashboard

--- cron.sh
+++ cron.sh
@@ -8,6 +8,7 @@ declare -A TIMES
 TIMES[imports]="*/30 * * * *"
 TIMES[scheduled-imports]="* * * * *"
 TIMES[dashboard-feeds]="*/5 * * * *"
+TIMES[dashboard-feeds-every-minute]="1-4,6-9,11-14,16-19,21-24,26-29,31-34,36-39,41-44,46-49,51-54,56-59 * * * *"
 TIMES[average-requests]="0 20 * * *"
 TIMES[alerts]="0 20 * * *"
 TIMES[dispute-mails]="0 8 * * *"
@@ -17,12 +18,14 @@ declare -A COMMANDS
 COMMANDS[imports]="php /project/bin/console app:launch:imports"
 COMMANDS[scheduled-imports]="php /project/bin/console app:launch:scheduled-imports"
 COMMANDS[dashboard-feeds]="php /project/bin/console app:feed:dashboards"
+COMMANDS[dashboard-feeds-every-minute]="php /project/bin/console app:feed:dashboards"
 COMMANDS[average-requests]="php /project/bin/console app:feed:average:requests"
 COMMANDS[alerts]="php /project/bin/console app:generate:alerts"
 COMMANDS[dispute-mails]="php /project/bin/console app:mails-litiges"
 COMMANDS[missions]="php /project/bin/console app:generate:mission"
 
 declare -A SPECIFICS
+SPECIFICS[dashboard-feeds-every-minute]="col1-prod col1-rec"
 SPECIFICS[dispute-mails]="scs1-prod scs1-rec"
 SPECIFICS[missions]="cl2-prod cl2-rec"
 
